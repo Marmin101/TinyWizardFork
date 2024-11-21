434f5d3855f66a3ef6f0bac077e53e7353d3965c
Quinton <quinncg.dev@gmail.com>
2024-11-21 14:24:14 -0500

    Candlestick Boss Improvements

--- Assets/Source/AI/BehaviorTree/Conditions/TargetHPIsAboveCondition.cs
+++ Assets/Source/AI/BehaviorTree/Conditions/TargetHPIsAboveCondition.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 namespace Quinn.AI.BehaviorTree
 {
 	[Serializable, Unity.Properties.GeneratePropertyBag]
-	[Condition(name: "Target HP is Above", story: "[Target] is Above [Health] Percent HP", category: "Conditions", id: "b27b489c112db159a165818ee46131b7")]
+	[Condition(name: "Target HP is Above", story: "[Target] HP is [Comparer] Than [Health] Percent", category: "Conditions", id: "b27b489c112db159a165818ee46131b7")]
 	public partial class TargetHPIsAboveCondition : Condition
 	{
+		public enum Comparison
+		{
+			Greater,
+			Lesser
+		}
+
 		[SerializeReference]
 		public BlackboardVariable<Health> Target;
 		[SerializeReference]
-		public BlackboardVariable<float> Health = new(0.5f);
+		public BlackboardVariable<Comparison> Comparer = new(Comparison.Lesser);
 		[SerializeReference]
-		public BlackboardVariable<bool> Invert = new(false);
+		public BlackboardVariable<float> Health = new(0.5f);
 
 		public override bool IsTrue()
 		{
-			if (Invert.Value)
+			if (Target.Value == null)
 			{
-				return Target.Value != null && Target.Value.Percent <= Health.Value;
+				return false;
 			}
 
-			return Target.Value != null && Target.Value.Percent > Health.Value;
+			if (Comparer.Value is Comparison.Greater)
+			{
+				return Target.Value.Percent > Health.Value;
+			}
+			else
+			{
+				return Target.Value.Percent <=	Health.Value;
+			}
 		}
 	}
 }

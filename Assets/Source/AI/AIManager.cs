b8deb0df13ce6f426f81c94457841d5aa1b77ed9
Marmin <38385813+Marmin101@users.noreply.github.com>
2025-03-20 11:28:10 -0400

    Refactor AIManager.cs to use Lists instead of HashSets. Add dependencies.

--- Assets/Source/AI/AIManager.cs
+++ Assets/Source/AI/AIManager.cs
@@ -9,17 +9,27 @@ namespace Quinn.AI
 
 		public IEnumerable<AIAgent> AllAgents => _allAgents;
 		public IEnumerable<AIAgent> EnvironmentAgents => _environmentAgents;
-		public IEnumerable<AIAgent> MonsterAgents => _environmentAgents;
-		public IEnumerable<AIAgent> CultistAAgents => _environmentAgents;
-		public IEnumerable<AIAgent> CultistBAgents => _environmentAgents;
+        // Fixed a bug where the following 3 enumerables were incorrectly set to _environmentAgents.
+        public IEnumerable<AIAgent> MonsterAgents => _monsterAgents;
+		public IEnumerable<AIAgent> CultistAAgents => _cultistAAgents;
+		public IEnumerable<AIAgent> CultistBAgents => _cultistBAgents;
 
-		private readonly HashSet<AIAgent> _allAgents = new();
-		private readonly HashSet<AIAgent> _environmentAgents = new();
-		private readonly HashSet<AIAgent> _monsterAgents = new();
-		private readonly HashSet<AIAgent> _cultistAAgents = new();
-		private readonly HashSet<AIAgent> _cultistBAgents = new();
+		private readonly List<AIAgent> _allAgents = new();
+		private readonly List<AIAgent> _environmentAgents = new();
+		private readonly List<AIAgent> _monsterAgents = new();
+		private readonly List<AIAgent> _cultistAAgents = new();
+		private readonly List<AIAgent> _cultistBAgents = new();
+        // These Lists used to be HashSets.
+        // In this case, HashSets are more efficient than lists,
+        // because they have time complexity O(1), whereas Lists have time complexity O(n).
+        // Time complexity O(1) means it is constant time, no matter the size of the container.
+        // Time complexity O(n) means that it is linear time, taking more time as the size of the container increases.
+        // In this game, there are not many enemies loaded at once, so there is no noticeable benefit to either-or.
+        // Lists in this context are also better because the order of the elements is important.
+        // HashSets do not maintain the order of the elements, whereas Lists do.
+		// The order is important to know which agents are being operated on (added or removed or indexed).
 
-		private void Awake()
+        private void Awake()
 		{
 			Debug.Assert(Instance == null);
 			Instance = this;
